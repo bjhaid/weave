b6224c97cad840ff7a9c8cd90b59a45783721cfd
Bryan Boreham <bjboreham@gmail.com>
2017-04-09 19:00:42 +0000

    Pass bridge name into awsvpc

--- prog/weaver/main.go
+++ prog/weaver/main.go
@@ -343,7 +343,7 @@ func main() {
 		var t tracker.LocalRangeTracker
 		if isAWSVPC {
 			Log.Infoln("Creating AWSVPC LocalRangeTracker")
-			t, err = tracker.NewAWSVPCTracker()
+			t, err = tracker.NewAWSVPCTracker(bridgeConfig.WeaveBridgeName)
 			if err != nil {
 				Log.Fatalf("Cannot create AWSVPC LocalRangeTracker: %s", err)
 			}
